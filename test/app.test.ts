f43e46b4c21a6619c2c65a6299b705ded8b734a8
WesleyVitor <wesleydemorais@outlook.com.br>
2022-04-21 19:53:24 -0300

    feat: Initial Tests

---
+++ test/app.test.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "@jest/globals";
+import supertest from "supertest";
+import app from "../src/index";
+describe("API root suite", () => {
+  test("should return object with value of msg key ok baby", async () => {
+    const response = await supertest(app).get("/");
+    expect(response.body.msg).toBe("Ok Baby");
+  });
+});
